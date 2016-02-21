393f71663766b0bdb0c89afe7ede9fdb8a04e134
kenshero <kenshero.k@gmail.com>
2016-02-21 15:57:05 +0700

    v3

---
+++ config/deploy.rb
@@ -0,0 +1,23 @@
+lock '3.4.0'
+
+
+set :application, 'nodecodeship'
+set :repo_url, 'git@github.com:kenshero/nodecodeship.git'
+set :deploy_to, '/home/ubuntu/codeshipnode'
+set :ssh_options, { :forward_agent => true }
+set :deploy_via, :copy
+# set :linked_files, %w{config/database.yml}
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+namespace :deploy do
+
+  desc 'Restart application'
+  # task :restart do
+  #   on roles(:app), in: :sequence, wait: 5 do
+  #     execute :touch, release_path.join('tmp/restart.txt')
+  #   end
+  # end
+
+  after :publishing, 'deploy:restart'
+  after :finishing, 'deploy:cleanup'
+end
